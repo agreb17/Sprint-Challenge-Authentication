c43ad835b18637bff15414a78660c5e928663e13
Anthony Greb <agreb255@gmail.com>
2018-10-19 12:26:43 -0400

    MVP

--- ANSWERS.md
+++ ANSWERS.md
@@ -1,9 +1,13 @@
 <!-- Answers to the Short Answer Essay Questions go here -->
 
 1. What is the purpose of using _sessions_?
+   It is a simple way to store data for users with a unique session ID.
 
 2. What does bcrypt do to help us store passwords in a secure manner.
+   it takes plain text passwords and encrypts them to make it harder to crack by adding salt to the hash, which increases the password strength.
 
 3. What does bcrypt do to slow down attackers?
+   it hashes the password which slows down the attackers because the more times the password is hashed the longer it takes to get the password.
 
 4. What are the three parts of the JSON Web Token?
+   the Header, the Payload, and the Signature
--- config/middlewares.js
+++ config/middlewares.js
@@ -1,27 +1,41 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
-const jwtKey = require('../_secrets/keys').jwtKey;
+const jwtKey = require("../_secrets/keys").jwtKey;
 
 // quickly see what this file exports
 module.exports = {
   authenticate,
+  generateToken
 };
 
 // implementation details
 function authenticate(req, res, next) {
-  const token = req.get('Authorization');
+  const token = req.get("Authorization");
 
   if (token) {
     jwt.verify(token, jwtKey, (err, decoded) => {
-      if (err) return res.status(401).json(err);
+      if (err) {
+        return res.status(401).json(err);
+      } else {
+        req.decoded = decoded;
 
-      req.decoded = decoded;
-
-      next();
+        next();
+      }
     });
   } else {
     return res.status(401).json({
-      error: 'No token provided, must be set on the Authorization Header',
+      error: "No token provided, must be set on the Authorization Header"
     });
   }
+}
+
+function generateToken(user) {
+  const jwtPayload = {
+    username: user.username
+  };
+  const jwtOptions = {
+    expiresIn: "10m"
+  };
+
+  return jwt.sign(jwtPayload, jwtKey, jwtOptions);
 }
